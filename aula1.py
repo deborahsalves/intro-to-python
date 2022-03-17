da80595681f03393c858e1bff80b38c9b3868a3b
deborahsalves <salves.deborah@gmail.com>
2022-03-16 21:01:51 -0300

    Initial commit

---
+++ aula1.py
@@ -0,0 +1,14 @@
+print("hello world")
+print(11//3)
+print(11%3)
+
+# hash to comment
+# atalho do teclado: ctrl + ;
+# e o undo é o mesmo atalho
+"""pra passar instruções pra outres devs, usa três aspas duplas"""
+# só cuida que ainda vai ser executado, mas não vai pro front :thinking:
+
+a = 2
+b = a + 1
+print(a + b)
+
